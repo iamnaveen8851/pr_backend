2cc0dc1b02fea25c8fac74f1cecbca7db26b837e
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-03-20 10:48:46 +0530

    url updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://pr-frontend-one.vercel.app/users/login",
+    origin: "https://pr-frontend-one.vercel.app",
     // origin: "http://localhost:5173", // change this to your frontend URL
     credentials: true, // enable set-cookie headers
   })
