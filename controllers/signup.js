a4acd7b592377e18097444c5df67945323862305
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-03-20 13:09:31 +0530

    access token added in signup route

--- controllers/signup.js
+++ controllers/signup.js
@@ -45,7 +45,7 @@ const signup = async (req, res) => {
 
       res
         .status(201)
-        .json({ message: "User created successfully", user: newUser });
+        .json({ message: "User created successfully", user: newUser, accessToken: token });
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
