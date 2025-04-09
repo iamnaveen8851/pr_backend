b14aa6beda888c327c022187b2214985fa126834
iamnaveen8851 <nk24528.nk81@gmail.com>
2025-04-09 11:55:40 +0530

    authorize roles added on taskAllocationRouter

--- server.js
+++ server.js
@@ -60,9 +60,9 @@ app.use(cookieParser());
 // cors configuration
 app.use(
   cors({
-    origin: "https://pr-frontend-one.vercel.app", // // PRO URL
+    // origin: "https://pr-frontend-one.vercel.app", // // PRO URL
 
-    // origin: "http://localhost:5173", // DEV URL
+    origin: "http://localhost:5173", // DEV URL
      // origin:  "https://ad26-2405-201-4021-1a06-4f9-9612-eb82-628a.ngrok-free.app", 
 
     credentials: true, // enable set-cookie headers
